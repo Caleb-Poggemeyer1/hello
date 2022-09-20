3ef379765313046e9d56023c02323b2d208ee871
Caleb Poggemeyer <cpoggemeyer2@huskers.unl.edu>
2022-09-20 19:30:38 +0000

    initial commit

--- hello.c
+++ hello.c
@@ -1,6 +1,21 @@
+/**
+ * Author: Caleb Poggemeyer
+ *
+ * Date: 09/20/2022
+ *
+ * A simple hello world program in C
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+printf("Owen Robb!\n");
+printf("My major is Computer Science.\n");
 printf("Caleb Poggemeyer!!!\n");
 printf("Computer Engineering\n");
-printf("Owen Robb!!!\n");
-printf("Computer Science\n");
 
+return 0;
 }
